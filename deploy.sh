e5b22663e7a4dcbfe60b197c3df9020f8313073d
Oleg Markoff <mawglih@gmail.com>
2019-03-17 21:39:35 -0400

    deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,4 @@ kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=mawglih/multi-server:$SHA
 kubectl set image deployments/client-deployment client=mawglih/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=mawglih/multi-worker:$SHA
+
